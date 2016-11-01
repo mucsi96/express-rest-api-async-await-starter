7e007ee04b7ccef525445752bc324312e5495bfb
Igor Mucsicska <mucsi_96@yahoo.com>
2016-11-01 16:43:45 +0100

    add async await support

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,16 @@
 import express from 'express'
 
 const app = express()
+const wrap = fn => (...args) => fn(...args).catch(args[2])
 
-app.get('/', (req, res) => {
-  res.send('Hello')
-})
+app.get('/', wrap(async (req, res) => {
+  const result = await new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Hello')
+    }, 500)
+  })
+  res.send(result)
+}))
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!')
