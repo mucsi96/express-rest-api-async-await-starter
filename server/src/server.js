590475cce62d1a026a883517601400c747a31f6b
Igor Mucsicska <mucsi_96@yahoo.com>
2016-11-01 19:08:26 +0100

    Add error handling

--- server/src/server.js
+++ server/src/server.js
@@ -5,6 +5,18 @@ const app = express()
 
 app.use(routes)
 
+app.use((req, res, next) => {
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+// error handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  res.send({'error': err.message})
+})
+
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!')
 })
